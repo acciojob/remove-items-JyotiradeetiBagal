f84a1c74ad270f569894082e497fc30da02821ef
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-08 15:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+let color=document.getElementById("colorSelect");
+let btn=document.getElementsByTagName("input")[0];
+
+btn.addEventListener("click",()=>{
+	const selectedIndex = color.selectedIndex; 
+	if (selectedIndex >= 0) {
+	    color.remove(selectedIndex); 
+	} 
+	else {
+	    alert("No option selected!");
+	}
+});
